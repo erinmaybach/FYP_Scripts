ae07194c32721ee8800c59f1dc04ce1a9c9d607b
Erin Maybach <110180552+erinmaybach@users.noreply.github.com>
2024-06-28 11:44:56 -0400

    Update megahitasmbl.sh

--- megahitasmbl.sh
+++ megahitasmbl.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-user=XXXX
 
 module load anaconda3
-conda activate /ocean/projects/bio210078p/emaybach/.conda/envs/project_1/
+conda activate /XXXX/.conda/envs/project_1/
 
 megahit -1 trimmedreads/SRR7822613_trimmed_1.fastq,trimmedreads/SRR7822620_trimmed_1.fastq,trimmedreads/SRR7822621_trimmed_1.fastq \
   -2 trimmedreads/SRR7822613_trimmed_2.fastq,trimmedreads/SRR7822620_trimmed_2.fastq,trimmedreads/SRR7822621_trimmed_2.fastq \
