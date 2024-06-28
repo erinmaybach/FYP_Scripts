f61f834fbbc9a3ef99b70caeb3ac421eab47221a
Erin Maybach <110180552+erinmaybach@users.noreply.github.com>
2024-06-28 11:42:16 -0400

    Update Sickle_npac.sh

--- Sickle_npac.sh
+++ Sickle_npac.sh
@@ -1,13 +1,13 @@
 
 #!/bin/sh
-#SBATCH --account=bio210078p
+#SBATCH --account=XXXX
 #SBATCH --job-name=Sickle_npac
 #SBATCH --partition=RM-shared
 #SBATCH --time=04:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mail-user=emaybach@ldeo.columbia.edu
+#SBATCH --mail-user=XXXX
 
 module load anaconda3
 cd /oceans/project/bio210078p/emaybach/MetaG_2015/01_RawReads_npac
