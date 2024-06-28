9609364d3b26a9b5ad482a5b171cfe7d8c664240
Erin Maybach <110180552+erinmaybach@users.noreply.github.com>
2024-06-28 11:16:21 -0400

    Create download_sra_fastq.sh

---
+++ download_sra_fastq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=bio210078p
+#SBATCH --job-name=downloadfastq
+#SBATCH --partition=RM-shared
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --output=downloadfastq_%j.txt
+#SBATCH --error=downloadfastq_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emaybach@ldeo.columbia.edu
+
+module load sra-toolkit/2.10.9
+
+vdb-config --prefetch-to-cwd
+
+for sample in $(cat samples.txt)
+do
+   prefetch "$sample" --max-size u
+   fastq-dump "$sample" --split-3 --skip-technical
+done
