30b664d3dd0528325705a916df75268da3ce70fc
Erin Maybach <110180552+erinmaybach@users.noreply.github.com>
2024-06-28 11:17:23 -0400

    Create trim.sh

---
+++ trim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=bio210078p
+#SBATCH --job-name=trim
+#SBATCH --partition=RM-shared
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --output=trim_%j.txt
+#SBATCH --error=trim_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emaybach@ldeo.columbia.edu
+
+module load anaconda3
+conda activate /ocean/projects/bio210078p/emaybach/.conda/envs/project_1/
+
+for sample in $(cat samples.txt)
+do
+  sickle pe \
+   -f rawreads/"$sample"_1.fastq \
+   -r rawreads/"$sample"_2.fastq \
+   -t sanger \
+   -o trimmedreads/"$sample"_trimmed_1.fastq \
+   -p trimmedreads/"$sample"_trimmed_2.fastq \
+   -s trimmedreads/"$sample"_singletons.fastq
+done
+
+conda deactivate
